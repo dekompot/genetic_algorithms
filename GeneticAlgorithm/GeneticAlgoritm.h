f2be125be75358427adeb3d673f394a7709b722d
dekompot <dekompot@gmail.com>
2023-01-17 17:32:20 +0100

    Initial commit

---
+++ GeneticAlgorithm/GeneticAlgoritm.h
@@ -0,0 +1,14 @@
+//
+// Created by 48791 on 13.01.2023.
+//
+
+#ifndef MINIPROJEKT_GENETICALGORITM_H
+#define MINIPROJEKT_GENETICALGORITM_H
+
+
+class GeneticAlgoritm {
+
+};
+
+
+#endif //MINIPROJEKT_GENETICALGORITM_H
---
+++ GeneticAlgorithm/Individual/Individual.h
@@ -0,0 +1,14 @@
+//
+// Created by 48791 on 13.01.2023.
+//
+
+#ifndef MINIPROJEKT_INDIVIDUAL_H
+#define MINIPROJEKT_INDIVIDUAL_H
+
+
+class Individual {
+
+};
+
+
+#endif //MINIPROJEKT_INDIVIDUAL_H
